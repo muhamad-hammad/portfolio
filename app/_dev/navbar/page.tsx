4d245aef0f563874356831c273a1483d613ecf36
Muhammad Hammad <m.hammad.bhatti09@gmail.com>
2026-05-08 16:29:55 +0500

    add private _dev/navbar route to preview the Navbar in isolation during styling iterations without polluting public routes

---
+++ app/_dev/navbar/page.tsx
@@ -0,0 +1,10 @@
+import { Navbar } from "@/components/Navbar";
+
+export default function NavbarDevPage() {
+  return (
+    <>
+      <Navbar />
+      <div style={{ height: "200vh" }} />
+    </>
+  );
+}
